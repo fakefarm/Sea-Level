2d8a210522a4f23225271880bb8a8685186e4cfc
Dave Woodall <dmwoodall@gmail.com>
2012-04-18 15:06:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeaLevel::Application.config.secret_token = '9b83ec10ae68e99dc8331bd26e6da4a65c288f6cec6e9d6159c51ef1b8467b50810679f5321dda3d676cfed3d59387c78c52e7ef5a294f294bd4bc2d316cea55'
